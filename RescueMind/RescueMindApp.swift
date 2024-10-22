47b36d4bf17013c4e7625eaae51eec7352010e6d
Muharrem Koroglu <muharrem-koroglu.99@hotmail.com>
2024-10-22 14:28:43 +0300

    Initial Commit

---
+++ RescueMind/RescueMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  RescueMindApp.swift
+//  RescueMind
+//
+//  Created by Muharrem Köroğlu on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct RescueMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
